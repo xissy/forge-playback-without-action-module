88e608a705a7c431ffe7695bf917e53fc0e322f5
Taeho Kim <xissysnd@gmail.com>
2013-11-11 20:56:06 -0800

    removed module build directory

--- inspector/ios-inspector/ForgeModule/build/ForgeModule.build/Release-iphonesimulator/ForgeModule.build/Objects-normal/i386/playback_without_action_API.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/xissysnd/recom.io/forge-modules/playback_without_action/inspector/ios-inspector/ForgeModule/playback_without_action_API.m \
-  /Users/xissysnd/recom.io/forge-modules/playback_without_action/inspector/ios-inspector/ForgeModule/playback_without_action_API.h
